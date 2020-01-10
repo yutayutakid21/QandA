d5520758edea9b51a40601325e27970628bc65cd
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-10 19:05:23 +0900

    question内容を追加

---
+++ QandA/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  QandA
+//
+//  Created by 宏輝 on 10/01/2020.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import Foundation
+
+class Question{
+    
+    var question1 = "swiftはiPhone、Androidのどちらのコードが書けるのですか？"
+    var question2 = "コアラが生息している国はどこですか？"
+    var question3 = "日本の車ハンドルはどちらですか？"
+}
