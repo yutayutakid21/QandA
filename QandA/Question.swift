78d5168cd5056f3728e1688195724927796792a9
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-11 21:45:42 +0900

    質問内容を追加

--- QandA/Question.swift
+++ QandA/Question.swift
@@ -27,17 +27,7 @@ class Question{
     
     
     
-    let question1 = "swiftはiPhone、Androidのどちらのコードが書けるのですか？"
-    let answer1a = "iPhone"
-    let answer1b = "Android"
-    
-    let question2 = "コアラが生息している国はどこですか？"
-    let answer2a = "日本"
-    let answer2b = "オーストラリア"
-    
-    let question3 = "日本の車ハンドルはどちらですか？"
-    let answer3a = "右"
-    let answer3b = "左"
+  
     
     
     
--- QandA/QuestionBank.swift
+++ QandA/QuestionBank.swift
@@ -8,3 +8,17 @@
 
 import Foundation
 
+class QuestionBank {
+    
+     var list = [Question]()
+    
+    init() {
+        
+        list.append(Question(questionText: "swiftはiPhone、Androidのどちらのコードが書けるのですか？", answer1: "iPhone", answer2: "Android", correctOrNot: 1))
+        list.append(Question(questionText: "コアラが生息している国はどこですか？", answer1: "日本", answer2: "オーストラリア", correctOrNot: 2))
+        list.append(Question(questionText: "日本の車ハンドルはどちらですか？", answer1: "右", answer2: "左", correctOrNot: 1))
+        
+    }
+    
+    
+}
